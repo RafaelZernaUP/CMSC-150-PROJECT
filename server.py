75d8d1bd923b747c5bc6049a19bfeb6a0d0bb743
Rafael Zerna <nvzerna@up.edu.ph>
2024-11-23 20:37:58 +0800

    fix: http server

--- server.py
+++ server.py
@@ -2,7 +2,9 @@ import http.server
 
 class handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
-        open('index.html')
+        file = open('index.html').read()
         self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(file, 'utf-8'))
 
 http.server.HTTPServer(('localhost',8080), handler).serve_forever()
