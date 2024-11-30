6c74149736d74841bc360691d5c2a15f25dda9bb
Rafael Zerna <nvzerna@up.edu.ph>
2024-11-30 19:39:12 +0800

    feat: enhanced matrix class

--- Classes/matrix.py
+++ Classes/matrix.py
@@ -1,34 +1,42 @@
 class matrix():
     def __init__(self, row, col):
-        self = []
+        __row:int = row
+        __col:int = col
+        __data:list = []
         for a in range(row):
-            self.append([])
+            __data.append([])
             for b in range(col):
-                self[a].append(0)
+                __data[a].append(0)
 
-    def printMatrix(matrix:list):
-        for i in matrix:
+    def print(self):
+        for i in self.__data:
             for j in i:
                 print(f"{j:.1f}", end="\t")
             print()
 
-    def printRow(list:list):
-        for i in list:
+    def printRow(self, row):
+        for i in self.__data[row]:
             print(f"{i:.1f}", end="\t")
         print()
 
-    def multiplyRow(row:list, x):
-        for a in range(len(row)):
-            row[a] *= x
+    def multiplyRow(self, row, x):
+        for a in range(len(self.__data[row])):
+            self[row][a] *= x
 
-    def divideRow(row:list, x):
-        for a in range(len(row)):
-            row[a] *= x
+    def divideRow(self, row, x):
+        for a in range(len(self.__data[row])):
+            self[row][a] /= x
 
-    def addRow(row:list, list:list):
-        for a in range(len(row)):
-            row[a] += list[a]
+    def addRow(self, row, x):
+        for a in range(len(self.__data[row])):
+            self[row][a] += x
 
-    def subtractRow(row:list, list:list):
-        for a in range(len(row)):
-            row[a] -= list[a]
+    def subtractRow(self, row, x):
+        for a in range(len(self.__data[row])):
+            self[row][a] -= x
+
+    def getRow(self):
+        return self.__row
+    
+    def getCol(self):
+        return self.__col
