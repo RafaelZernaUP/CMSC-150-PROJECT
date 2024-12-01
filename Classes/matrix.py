4d776c27b8f2dfaea183bbe53bf7a506b86f5e6c
Rafael Zerna <nvzerna@up.edu.ph>
2024-12-01 14:47:25 +0800

    doc: matrix.py

--- Classes/matrix.py
+++ Classes/matrix.py
@@ -1,56 +1,84 @@
+
 # Distance between columns when printing
 DIST = ""
 
+
+
+# Matrix Class
+
 class matrix():
 
+    # Constructor
     def __init__(self, row, col):
+
+        # Sets rows and columns
         self.__row:int = row
         self.__col:int = col
+
+        # Initializes all matrix elements to 0
         self.__data:list[list[float]] = []
         for a in range(row):
             self.__data.append([])
             for b in range(col):
                 self.__data[a].append(0)
 
+
+
+    # Print methods for matrices and rows
     def printMatrix(self):
         for i in self.__data:
             matrix.printRow(i)
             print()
-
     def printRow(row:list[float]):
         for i in row:
             print(f"{i:10.2f}", end=DIST)
         print()
+    
 
+
+    # Row operations
+
+    # Multiply row by a constant
     def multiplyRow(row:list[float], x:float):
         for a in range(len(row)):
             row[a] *= x
         return row
-
+    
+    # Divide row by a constant
     def divideRow(row:list[float], x:float):
         for a in range(len(row)):
             row[a] /= x
         return row
 
+    # Add a constant to a row
     def addRow(row:list[float], x:float):
         for a in range(len(row)):
             row[a] += x
         return row
 
+    # Subtract a constant from a row
     def subtractRow(row:list[float], x:float):
         for a in range(len(row)):
             row[a] -= x
         return row
 
+    # Adds two rows
     def addRowToRow(row:list[float], x:list[float]):
         for a in range(len(row)):
             row[a] += x[a]
         return row
 
+    # Subtracts a row from a row
     def subtractRowFromRow(row:list[float], x:list[float]):
         for a in range(len(row)):
             row[a] -= x[a]
         return row
+    
+    # END OF ROW OPERATIONS
+
+
+
+    # Getter and setter methods
 
     def getRowNum(self):
         return self.__row
@@ -88,3 +116,5 @@ class matrix():
             index = self.__row + index
         for a in range(len(self.__data)):
             self.__data[a][index] = col[a]
+
+    # END OF GETTER AND SETTER METHODS
